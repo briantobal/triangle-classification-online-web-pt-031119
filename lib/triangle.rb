bebc105f1613dbba307aa7cd0d647ce9b8dfdace
Brian Tobal <brian.tobal@gmail.com>
2019-04-24 12:06:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,29 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene, :kind
-  
-  def initialize(s1, s2, s3)   
-    @s1 = s1 
-    @s2 = s2 
-    @s3 = s3 
-  end     #def 
-  
-  def kind  
-    if (@s1*@s2*@s3 = 0) || (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2 
-     
-     begin  
-      raise TriangleError
-      puts error.message
-    end 
-    
-    elsif @s1 == @s2 && @s1 == @s3
-      self.kind = :equilateral
-  
-    elsif s1 == s2 || s1 == s3 || s2 == s3
-      self.kind = :isosceles
-      
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      self.kind = :scalene
-    
-    end 
-  end     #def 
-  
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
   class TriangleError < StandardError
-    def message 
-      puts "not a triangle"
-  end     #def 
-  end 
+  end
 
 end
