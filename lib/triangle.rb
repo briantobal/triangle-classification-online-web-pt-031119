1635d6b54291baf70fbacb78e4011ccd2dba49da
Brian Tobal <brian.tobal@gmail.com>
2019-04-24 11:37:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene 
   
-  def initialize(s1, s2, s3)     
+  def initialize(s1, s2, s3)   
+    @s1 = s1 
+    @s2 = s2 
+    @s3 = s3 
   end     #def 
 end
